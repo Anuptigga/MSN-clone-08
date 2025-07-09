cd45c2817e56073593c1f4a2d17436c30753748b
Anup <anuptigga21@gmail.com>
2025-07-09 12:36:46 +0530

    added filter and category in news

--- backend/models/News.js
+++ backend/models/News.js
@@ -4,6 +4,7 @@ const newsSchema = new mongoose.Schema({
     title:{type:String,required:true},
     description:{type:String, required:true},
     image:{type:String,required:true},
+    category:{type:String, enum:['Sports','Politics','Tech','Entertainment','Shopping'], required:true},
     isTrending:{type:Boolean,default:false}
 },
 {timestamps:true}
